94711b1e3721f7fe388b85c9df1d6457e19ef9be
vaks <vaks@hotmail.es>
2018-02-12 16:36:21 +0100

    Enlazado el menu y empezada la pantalla settings

---
+++ app/src/main/java/dsm/servabo/wwbm/PlayActivity.java
@@ -0,0 +1,15 @@
+package dsm.servabo.wwbm;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by servabo on 12/02/2018.
+ */
+
+public class PlayActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+    }
+}
