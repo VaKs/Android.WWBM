0ea20a0b24a29d1b5608beb4b5c1cfb693c9546e
Sergio Vacas <vaks@hotmail.es>
2018-02-27 19:14:08 +0100

    SettingsActivity ya guarda SharedPreferences

--- app/src/main/java/dsm/servabo/wwbm/SettingsActivity.java
+++ app/src/main/java/dsm/servabo/wwbm/SettingsActivity.java
@@ -1,8 +1,8 @@
 package dsm.servabo.wwbm;
 
+
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -13,6 +13,9 @@ import android.widget.Spinner;
  */
 
 public class SettingsActivity extends AppCompatActivity {
+
+    SharedPreferences prefs;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
@@ -21,8 +24,7 @@ public class SettingsActivity extends AppCompatActivity {
         Spinner spinner = (Spinner) findViewById(R.id.spnNumber);
         String[] letra = {"0","1","2","3"};
         spinner.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item, letra));
-
-
+        prefs = getApplicationContext().getSharedPreferences("SharedPreferencesWWBM", MODE_PRIVATE);
     }
 
     @Override
@@ -30,14 +32,16 @@ public class SettingsActivity extends AppCompatActivity {
 
         EditText name = findViewById(R.id.ptEnterName);
         Spinner ayudas = findViewById(R.id.spnNumber);
-        SharedPreferences prefs =  PreferenceManager. getDefaultSharedPreferences (this );
         SharedPreferences.Editor editor = prefs.edit();
 
-        editor.putString("Nombre",name.toString());
-        editor.putInt("Ayudas", ((int) ayudas.getSelectedItem()));
+        editor.clear();
+
+        editor.putString("Nombre",name.getText().toString());
+        editor.putInt("Ayudas", (Integer.parseInt(ayudas.getSelectedItem().toString())));
 
         editor.apply();
         super.onPause();
+
     }
 
     @Override
@@ -45,12 +49,11 @@ public class SettingsActivity extends AppCompatActivity {
 
         EditText name = findViewById(R.id.ptEnterName);
         Spinner ayudas = findViewById(R.id.spnNumber);
-        SharedPreferences prefs =  PreferenceManager. getDefaultSharedPreferences (this );
 
-        name.setText(prefs.getString("Nombre",""));
-        ayudas.setSelection(prefs.getInt("Ayudas", 0));
-
+        name.setText(prefs.getString("Nombre","aaa"));
+        ayudas.setSelection(prefs.getInt("Ayudas", 1));
         super.onResume ();
+
     }
 
 
