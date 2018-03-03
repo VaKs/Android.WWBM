6559871e45313834a06ef7eda3e1907887f224d8
David Guillén Mateu <davidguillenmateu95@gmail.com>
2018-03-03 01:04:10 +0100

    Se llena el questionList correctamente

--- app/src/main/java/dsm/servabo/wwbm/PlayActivity.java
+++ app/src/main/java/dsm/servabo/wwbm/PlayActivity.java
@@ -2,13 +2,18 @@ package dsm.servabo.wwbm;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -16,45 +21,39 @@ import javax.xml.parsers.DocumentBuilderFactory;
 
 import POJO.Question;
 
-/**
- * Created by servabo on 12/02/2018.
- */
-
 
 public class PlayActivity extends AppCompatActivity {
+    ArrayList<Question> questionList = new ArrayList<>();
+    Integer premio = 0;
+    Integer sigPremio = 1;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
-        try {
-            File xmlFile = (File) getResources().getXml(R.xml.questions);
-            ArrayList<Question> questionList = new ArrayList<>();
-            Question question = new Question();
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(xmlFile);
-            doc.getDocumentElement().normalize();
-            NodeList nList = doc.getElementsByTagName("question");
-            for(int temp =0; temp<nList.getLength();temp++){
-                Node nNode =nList.item(temp);
-                if(nNode.getNodeType() == Node.ELEMENT_NODE){
-                    Element eElement = (Element) nNode;
-                    question.setAnswer1(eElement.getAttribute("answer1"));
-                    question.setAnswer2(eElement.getAttribute("answer2"));
-                    question.setAnswer3(eElement.getAttribute("answer3"));
-                    question.setAnswer4(eElement.getAttribute("answer4"));
-                    question.setAudience(eElement.getAttribute("audience"));
-                    question.setFifty1(eElement.getAttribute("fifty1"));
-                    question.setFifty2(eElement.getAttribute("fifty2"));
-                    question.setNumber(eElement.getAttribute("number"));
-                    question.setPhone(eElement.getAttribute("phone"));
-                    question.setRight(eElement.getAttribute("right"));
-                    question.setText(eElement.getAttribute("text"));
+        try{
+            XmlPullParser parser = getResources().getXml(R.xml.questions);
+            while(parser.getEventType() != XmlPullParser.END_DOCUMENT){
+                int event = parser.next();
+                Question question = new Question();
+                if(event == XmlPullParser.START_TAG) {
+                    question.setAnswer1(parser.getAttributeValue(null, "answer1"));
+                    question.setAnswer2(parser.getAttributeValue(null, "answer2"));
+                    question.setAnswer3(parser.getAttributeValue(null, "answer3"));
+                    question.setAnswer4(parser.getAttributeValue(null, "answer4"));
+                    question.setAudience(parser.getAttributeValue(null, "audience"));
+                    question.setFifty1(parser.getAttributeValue(null, "fifty1"));
+                    question.setFifty2(parser.getAttributeValue(null, "fifty2"));
+                    question.setNumber(parser.getAttributeValue(null, "number"));
+                    question.setPhone(parser.getAttributeValue(null, "phone"));
+                    question.setRight(parser.getAttributeValue(null, "right"));
+                    question.setText(parser.getAttributeValue(null, "text"));
+                    questionList.add(question);
                 }
-
             }
-        }catch(Exception e){
-            e.printStackTrace();
         }
-
+        catch(XmlPullParserException e){e.printStackTrace();}
+        catch(IOException e){e.printStackTrace();}
+        questionList.remove(0);
+        TextView txtPremio = (TextView) findViewById(R.id.txtPremio);
+        txtPremio.setText(sigPremio.toString());
     }
 }
